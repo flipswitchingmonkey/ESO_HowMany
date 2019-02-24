5c3fa7321685cc966dcab3d9d39c83cd23c155d1
Michael Auerswald <michael.auerswald@gmail.com>
2019-02-24 23:29:44 +0100

    Update HowMany.lua
    Improved readability by leaving out containers that contain 0 of the item.

--- HowMany.lua
+++ HowMany.lua
@@ -8,12 +8,15 @@ function HowMany:Initialize()
 end
 
 function HowMany.OnLootReceived(eventCode, lootedBy, itemLink, quantity, itemSound, lootType, isStolen)
---  d("OnLootReceived")
   inventoryCount, bankCount, craftBagCount = GetItemLinkStacks(itemLink)
-  s = zo_strformat("Looted: <<1>>x <<2>> (Bag:<<3>> Bank:<<4>> Crafting:<<5>>)", quantity, itemLink, inventoryCount, bankCount, craftBagCount)
+  iCount = ""
+  bCount = ""
+  cCount = ""
+  if inventoryCount > 0 then iCount = zo_strformat("Inventory:<<1>> ",inventoryCount) end
+  if bankCount > 0 then bCount = zo_strformat("Bank:<<1>> ",bankCount) end
+  if craftBagCount > 0 then cCount = zo_strformat("CraftingBag:<<1>> ",craftBagCount) end
+  s = zo_strformat("Looted: <<1>>x <<2>> <<3>><<4>><<5>>", quantity, itemLink, iCount, bCount, cCount)
   CHAT_SYSTEM:AddMessage(s)
---  d(s)
---  d("Looted: " .. quantity .. "x " .. GetItemLinkName(itemLink) .. "(Quality:" .. GetItemLinkQuality(itemLink) .. ")" ..  "(I:"..inventoryCount.."|B:"..bankCount.."|C:"..craftBagCount..")")
 end
 
 function HowMany.OnAddOnLoaded(event, addonName)
