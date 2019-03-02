f43aa930b4b1ff007037e66e6ecc45d539110b0e
Richard Köhl <richard@koehls.de>
2019-03-02 10:54:31 +0100

    rename "Inventory" to shorter word "Bags"

--- HowMany.lua
+++ HowMany.lua
@@ -61,7 +61,7 @@ function HowMany.OnLootReceived(eventCode, lootedBy, itemLink, quantity, itemSou
     iCount = ""
     bCount = ""
     cCount = ""
-    if inventoryCount > 0 then iCount = zo_strformat("Inventory: <<1>> ",inventoryCount) end
+    if inventoryCount > 0 then iCount = zo_strformat("Bags: <<1>> ",inventoryCount) end
     if bankCount > 0 then bCount = zo_strformat("Bank: <<1>> ",bankCount) end
     if craftBagCount > 0 then cCount = zo_strformat("CraftingBag: <<1>> ",craftBagCount) end
     s = zo_strformat("Looted: <<1>>x <<2>> - <<3>><<4>><<5>>", quantity, itemLink, iCount, bCount, cCount)
